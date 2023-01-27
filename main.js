e8180c02b8137ade0b30eea29d77db3b44de87c7
Kunj1234567890 <97776880+Kunj1234567890@users.noreply.github.com>
2023-01-27 15:27:49 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+//https://teachablemachine.withgoogle.com/models/KbltBca8S//
+Webcam.attach('#camera');
+camera= document.getElementById("camera");
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+})
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img src="'+data_uri+'" id="sibling_image "> '
+
+    })
+}
+console.log('ml5version', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/KbltBca8S/model.json', modelLoaded);
+function modelLoaded(){
+    console.log("model is loaded")
+} 
+function check(){
+    img = document.getElementById('sibling_image');
+classifier.classify(img, gotResult); 
+
+}
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
